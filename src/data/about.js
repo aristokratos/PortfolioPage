0f3af40b6eb179db6a82d806bec930a415d7273a
aristokratos <oluwatobiloba.onawale.decagon.dev>
2022-12-19 18:47:22 +0100

    initial details commit

---
+++ src/data/about.js
@@ -0,0 +1,35 @@
+const about = [
+    {
+        key: 1,
+        img:"https://github.com/ssahibsingh/personalWebsite/blob/main/src/assets/backend2.png?raw=true",
+        name: "Backend Development",
+        description: "I am a backend developer with a passion for designing and implementing server-side components of software application.",
+        h1: "Technologies I use:",
+        h1Array: ['.NET Core', 'ASP.NET Core', 'SQL Server Database'],
+        h2: "Dev Tools:",
+        h2Array: ['VisualStudio', 'SwaggerUI', 'Terminal', 'Git', 'Github', 'Heroku']
+    },
+    {
+        key: 2,
+        img:"https://github.com/ssahibsingh/personalWebsite/blob/main/src/assets/frontend2.png?raw=true",
+        name: "Frontend Developement",
+        description: "I do frontend development to give user interface to my backend projects.",
+        h1: "Technologies I use:",
+        h1Array: ['HTML', 'CSS', 'JS', 'React.js'],
+        h2: "Dev Tools:",
+        h2Array: ['VSCode', 'Bootstrap', 'Terminal', 'Git', 'Github', 'CodeSandbox']
+    },
+    {
+        key: 3,
+        img:"https://github.com/ssahibsingh/personalWebsite/blob/main/src/assets/frontend2.png?raw=true",
+        name: "Game Developement",
+        description: "I excited about 2D/3D game development.",
+        h1: "Technologies I use:",
+        h1Array: ['.NET Core', 'C#', 'C++'],
+        h2: "Dev Tools:",
+        h2Array: ['VSCode', 'Visual Studio', 'Terminal', 'Git', 'Github']
+    },
+    
+]
+
+export default about
---
+++ src/data/education.js
@@ -0,0 +1,20 @@
+const education = [
+    {
+        key: 1,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-0b0JV4GVNDJ7VPb5UiiNAXy-RN8ohxj0EQ&usqp=CAU?raw=true",
+        name: "Decagon Institute",
+        duration: "2022 - Present",
+        education: "Software Engineering",
+    },
+    {
+        key: 2,
+        img: "https://www.ui.edu.ng/sites/default/files/UI%20logo.png?raw=true",
+        name: "University of Ibadan",
+        duration: "2017 - 2020",
+        education: "Bachelor of Science",
+        degree: "Issued by Central Board of Secondary Education (CBSE)",
+    }
+]
+
+
+export default education
